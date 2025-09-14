c7af35042db1ac222f516ddb3c295ab91474898b
Ibad Desmukh <ibaddesmukh@gmail.com>
2025-09-14 22:44:46 +0100

    Add basic LSTM model

--- model.py
+++ model.py
@@ -2,4 +2,11 @@ import torch
 import torch.nn as nn
 
 class SolarPowerPredictionLSTM(nn.Module):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=4, hidden_size=64, num_layers=2, batch_first=True)
+        self.fc = nn.Linear(64, 1)
+
+    def forward(self, x):
+        lstm_out, _ = self.lstm(x)
+        return self.fc(lstm_out[:, -1, :])
--- test_model.py
+++ test_model.py
@@ -1,6 +1,15 @@
 import pytest
 import torch
 
+from model import SolarPowerPredictionLSTM
+
 def test_model_exists():
-    from model import SolarPowerPredictionLSTM
-    assert SolarPowerPredictionLSTM is not None
+    """Test if SolarPowerPredictionLSTM can be run"""
+    
+    model = SolarPowerPredictionLSTM()
+
+    test_input = torch.randn(2, 12, 4)
+
+    output = model(test_input)
+
+    assert output.shape == (2, 1)
